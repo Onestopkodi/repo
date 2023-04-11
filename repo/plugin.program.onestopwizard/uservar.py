ba52e4d8121be0bbb55f40c92df1624046d2916c
OneStop <24619077+Onestopkodi@users.noreply.github.com>
2023-04-11 01:10:43 -0400

    updates

---
+++ repo/plugin.program.onestopwizard/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://www.dropbox.com/s/92qw6gxx80msakg/builds.xml?dl=1'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://www.dropbox.com/s/urbbqiq9883usyo/notify.txt?dl=1'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
