8314c78997b97f6afcc9e022a799794c087354f7
OneStop <24619077+Onestopkodi@users.noreply.github.com>
2023-10-30 18:09:17 -0400

    Update uservar.py

--- repo/plugin.program.onestopwizard/uservar.py
+++ repo/plugin.program.onestopwizard/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://www.dropbox.com/s/92qw6gxx80msakg/builds.xml?dl=1'
+buildfile = 'https://www.dropbox.com/s/2l5cwznsafd6fb1/builds.txt?dl=1'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'https://www.dropbox.com/s/urbbqiq9883usyo/notify.txt?dl=1'
