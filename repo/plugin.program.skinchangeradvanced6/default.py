100299ce52496dbed9b4b1d7f37baba0626b1041
OneStop <24619077+Onestopkodi@users.noreply.github.com>
2023-12-26 03:21:39 -0500

    update

--- repo/plugin.program.skinchangeradvanced6/default.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import xbmc, xbmcgui
-
-
-xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skin","value":"skin.estuary"}}')
-xbmc.executebuiltin('SendClick(11)')
-xbmc.sleep(500)
-xbmc.executebuiltin( 'ActivateWindow(Home)' )
